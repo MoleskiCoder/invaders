bdf2af14b5555ccf419f30afd0d2d35978e2ff8c
Adrian.Conlon <adrian.conlon@gmail.com>
2017-03-22 19:15:10 +0000

    Add preliminary (and skeletal!) support for SDL_mixer
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- src/stdafx.h
+++ src/stdafx.h
@@ -15,8 +15,10 @@
 #include <bitset>
 
 #include <SDL.h>
+#include <SDL_mixer.h>
 
 #ifdef _MSC_VER
 #pragma comment(lib, "SDL2.lib")
 #pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_mixer.lib")
 #endif
